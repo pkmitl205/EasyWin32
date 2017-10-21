6cedc972fe310bb644754435702227e147dca790
pkmitl205 <58030205@kmitl.ac.th>
2017-10-22 06:35:13 +0700

    Edited by Pakon

--- PrimarySurface/PrimarySurface/main.cpp
+++ PrimarySurface/PrimarySurface/main.cpp
@@ -6,23 +6,66 @@
 LPDIRECTDRAW7 dd = NULL;
 LPDIRECTDRAWSURFACE7 primary = NULL;
 
+LPDIRECTDRAWSURFACE7 GetBmp(LPDIRECTDRAW7 directdraw, LPCTSTR fileName)
+{
+	HDC hdc;
+	HBITMAP bit;
+	LPDIRECTDRAWSURFACE7 surf;
+
+	bit = (HBITMAP)LoadImage(NULL, fileName, IMAGE_BITMAP, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
+
+	if (!bit) return NULL;
+
+	BITMAP bitmap;
+	GetObject(bit, sizeof(BITMAP), &bitmap);
+	int surf_width = bitmap.bmWidth;
+	int surf_height =  bitmap.bmHeight;
+
+	HRESULT result;
+	DDSURFACEDESC2 ddsd;
+	ZeroMemory(&ddsd, sizeof(ddsd));
+	ddsd.dwSize = sizeof(DDSURFACEDESC2);
+	ddsd.dwFlags = DDSD_CAPS | DDSD_WIDTH | DDSD_HEIGHT;
+	ddsd.ddsCaps.dwCaps = DDSCAPS_OFFSCREENPLAIN | DDSCAPS_SYSTEMMEMORY;
+	ddsd.dwWidth = surf_width;
+	ddsd.dwHeight = surf_height;
+	result = directdraw->CreateSurface(&ddsd, &surf, NULL);
+	
+	if (result != DD_OK)
+	{
+		DeleteObject(bit);
+		return NULL;
+	}
+	else
+		{
+		surf->GetDC(&hdc);
+		HDC bit_dc = CreateCompatibleDC(hdc);
+		SelectObject(bit_dc, bit);
+		BitBlt(hdc, 0, 0, surf_width, surf_height, bit_dc, 0, 0, SRCCOPY);
+
+		surf->ReleaseDC(hdc);
+		DeleteDC(bit_dc);
+		}
+	return surf;
+}
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
 	switch (msg)
 	{
-	case WM_PAINT:
-	{
+		case WM_PAINT:
+		{
 
-	}break;
+		}break;
 
-	case WM_DESTROY:
-	{
-		primary->Release();
-		primary = NULL;
-		dd->Release();
-		dd = NULL;
-		PostQuitMessage(0);
-	}break;
+		case WM_DESTROY:
+		{
+			primary->Release();
+			primary = NULL;
+			dd->Release();
+			dd = NULL;
+			PostQuitMessage(0);
+		}break;
 	}
 	return (DefWindowProc(hwnd, msg, wparam, lparam));
 }
@@ -67,12 +110,16 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprevinstance, LPSTR lpcmdline
 
 	dd->CreateSurface(&surf, &primary, NULL);
 	
-	HDC hdc;
-	hdc = GetDC(hwnd);
-	primary->GetDC(&hdc);
-	TextOut(hdc, 100, 100, "Hello", 5);
-	primary->ReleaseDC(hdc);
+	RECT r;
+	r.left = 100;
+	r.top = 100;
+	r.right = r.left + 100;
+	r.bottom = r.top + 120;;
 
+//	primary->Blt(&r, GetBmp(dd, "SR01.bmp"), NULL, DDBLT_WAIT, NULL);
+
+	RECT dest_r;
+	int x = -50;
 
 	while (1)
 	{
@@ -86,6 +133,13 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprevinstance, LPSTR lpcmdline
 			else
 			{
 				//Game Main
+				x += 3;
+				dest_r.left = x;
+				dest_r.top = 100;
+				dest_r.right = dest_r.left + 105;
+				dest_r.bottom = dest_r.top + 115;
+
+				primary->Blt(&dest_r, GetBmp(dd, "SR01.bmp"), NULL, DDBLT_WAIT, NULL);
 
 			}
 	}
