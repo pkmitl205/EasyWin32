cb19f2d009819c3e5818019eb73a02efd8ed1ada
pkmitl205 <58030205@kmitl.ac.th>
2017-10-22 03:41:13 +0700

    Edited by Pakon

---
+++ Easy2/Easy2/Easy2.cpp
@@ -0,0 +1,70 @@
+#include <Windows.h>
+#include <windowsx.h>
+#include "resource.h"
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch (msg)
+	{
+	case WM_PAINT:
+	{
+
+	}break;
+
+	case WM_COMMAND:
+	{
+
+		switch (wparam)
+		{
+		case ID_FILE_EXIT: DestroyWindow(hwnd); break;
+		case ID_FILE_LOAD: MessageBox(hwnd, "LOAD", "", MB_OK);
+		}
+	}break;
+
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+	}break;
+	}
+	return (DefWindowProc(hwnd, msg, wparam, lparam));
+}
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprevinstance, LPSTR lpcmdline, int ncmdshow)
+{
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_VREDRAW | CS_HREDRAW | CS_OWNDC | CS_DBLCLKS;
+	wcex.lpfnWndProc = WindowProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hinstance;
+
+	wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wcex.lpszClassName = "MyWin";
+	wcex.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
+	wcex.hIcon = LoadIcon(hinstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(hinstance, IDC_ARROW);
+	wcex.hIconSm = LoadIcon(hinstance, IDI_APPLICATION);
+
+	RegisterClassEx(&wcex);
+
+	HWND hwnd;
+	MSG msg;
+
+	hwnd = CreateWindowEx(NULL, "MyWin", "Win32 Window", WS_OVERLAPPEDWINDOW, 100, 100, 400, 400, NULL, NULL, hinstance, NULL);
+
+	ShowWindow(hwnd, ncmdshow);
+	UpdateWindow(hwnd);
+
+	while (1)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
+		{
+			if (!GetMessage(&msg, NULL, 0, 0))
+				return msg.wParam;
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	return (msg.wParam);
+}
---
+++ Easy2/Easy2/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Easy2.rc
+//
+#define IDR_MAINMENU                    4
+#define IDR_MENU1                       4
+#define ID_FILE_LOAD                    40001
+#define ID_FILE_EXIT                    40002
+#define ID_FILE                         40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
